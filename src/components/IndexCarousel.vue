635b336c517fccdc69b7b2e30fb3c97868124f3d
SiHoonChris <chrisleegpock@gmail.com>
2023-09-03 18:43:09 +0900

    2nd commit : Modifying Carousel - but in error

--- src/components/IndexCarousel.vue
+++ src/components/IndexCarousel.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="displayer">
+    <!-- class명 부여한 것도 inline-css로 적용됨-그래서 이동 과정 중에 css효과 깨짐, 색상/폰트 부여는 JS로 처리해야할 듯 -->
     <div id="contents">
-      <div><p>DJI</p><p>34,346.90 <span class="plus">▲247.48 +0.73%</span></p></div>
       <div><p>USD/KRW</p><p>1,327.00 <span class="minus">▼2.00 -0.15%</span></p></div>
       <div><p>CNY/KRW</p><p>181.89 <span class="minus">▼0.62 -0.34%</span></p></div>
       <div><p>HKD/KRW</p><p>168.87 <span class="minus">▼0.29 -0.17%</span></p></div>
@@ -11,6 +11,7 @@
       <div><p>KOSPI</p><p>2,519.14 <span class="minus">▼18.54 -0.73%</span></p></div>
       <div><p>NASDAQ</p><p>13,590.65 <span class="plus">▲126.67 +0.94%</span></p></div>
       <div><p>S&P500</p><p>4,405.71 <span class="plus">▲29.40 +0.67%</span></p></div>
+      <div><p>DJI</p><p>34,346.90 <span class="plus">▲247.48 +0.73%</span></p></div>
     </div>
   </nav>
 </template>
@@ -18,34 +19,55 @@
 <script>
 export default {
   mounted(){
-    setInterval(()=>{
-      try {
-        const contents = document.querySelectorAll('#contents div')
-        const clone = document.querySelector('#contents div:first-child').cloneNode(true)
+    const contents = document.querySelectorAll('#contents div');
+    for(const i in contents) {
+      setInterval(()=>{
         setTimeout(()=>{
-          for(const i in contents){
-            if(i>0){
-              contents[i].style.transform="translateX(-14vw)"
-              contents[i].style.transition="2.0s linear"
-            }
-          }
+          contents[i].removeAttribute('style');
+          contents[i].style.transform="translateX(-" + String(14*(i+1)) + "vw)";
+          contents[i].style.transition = String(2*(i+1)) + "s linear";
         });
         setTimeout(()=>{
-          try {
-            document.querySelector("#contents").appendChild(clone)
-            contents[0].remove()
-            for(const i in contents){
-              if(i>0) contents[i].removeAttribute('style')
-            }
-          } catch(err) {
-            console.log(err)
-          }
-        }, 2000)
-      }
-      catch(e) {
-        console.log("IGNORE THIS ERROR : " + e);
-      }
-    }, 4000);
+          contents[i].removeAttribute('style');
+          contents[i].style.transform="translateX(" + String(14*(i+10)) + "vw)";
+        }, 2000);
+        setTimeout(()=>{
+          contents[i].removeAttribute('style');
+          contents[i].style.transform="translateX(-" + String(14*(i+1)) + "vw)";
+          contents[i].style.transition = String(2*(i+10)) + "s linear";
+          contents[i].removeAttribute('style');
+        }, 2100);
+      }, 20000);
+    }
+
+    // setInterval(()=>{
+    //   try {
+    //     const contents = document.querySelectorAll('#contents div')
+    //     const clone = document.querySelector('#contents div:first-child').cloneNode(true)
+    //     setTimeout(()=>{
+    //       for(const i in contents){
+    //         if(i>0){
+    //           contents[i].style.transform="translateX(-14vw)"
+    //           contents[i].style.transition="2.0s linear"
+    //         }
+    //       }
+    //     });
+    //     setTimeout(()=>{
+    //       try {
+    //         document.querySelector("#contents").appendChild(clone)
+    //         contents[0].remove()
+    //         for(const i in contents){
+    //           if(i>0) contents[i].removeAttribute('style')
+    //         }
+    //       } catch(err) {
+    //         console.log(err)
+    //       }
+    //     }, 2000)
+    //   }
+    //   catch(e) {
+    //     console.log("IGNORE THIS ERROR : " + e);
+    //   }
+    // }, 4000);
   }
 }
 </script>
@@ -58,7 +80,7 @@ export default {
     height: 8vh;
     display: flex;
     justify-content: left;
-    transform: translateX(-14vw);
+    /* transform: translateX(-14vw); */
 }
 #contents div {
     width: 14vw;
